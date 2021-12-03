33c6d010c5ed0fa146c44578a01f4b1747ecebfd
monyone <monyone.teihen@gmail.com>
2021-12-04 08:03:57 +0900

    remove unused export

--- src/embedded.ts
+++ src/embedded.ts
@@ -4,4 +4,4 @@ import CanvasProvider from './canvas-provider'
 import EmbeddedGlyph from './constants/mapping/additional-symbols'
 CanvasProvider.setEmbeddedGlyph(EmbeddedGlyph);
 
-export {EmbeddedGlyph, CanvasRenderer, CanvasProvider}
+export {CanvasRenderer, CanvasProvider}
