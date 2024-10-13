ac1544e836f3dadce75ea3a45697a7026d68b857
monyone <monyone.teihen@gmail.com>
2024-10-13 16:12:03 +0900

    Feat: Remove v1 export

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,3 @@
-export { default as CanvasRenderer } from './v1/canvas-renderer'
-export { default as CanvasProvider } from './v1/canvas-provider'
-export type { RendererOption as CanvasRendererOption } from './v1/canvas-renderer'
-export type { ProviderOption as CanvasProviderOption, ProviderResult as CanvasProviderResult} from './v1/canvas-provider'
-
-export { default as SVGRenderer } from './v1/svg-renderer'
-export { default as SVGProvider } from './v1/svg-provider'
-export type { RendererOption as SVGRendererOption } from './v1/svg-renderer'
-export type { ProviderOption as SVGProviderOption, ProviderResult as SVGProviderResult } from './v1/svg-provider'
-
-export { default as HTMLRenderer } from './v1/html-renderer-experimental'
-export { default as HTMLProvider } from './v1/html-provider-experimental'
-export type { RendererOption as HTMLRendererOption } from './v1/html-renderer-experimental'
-export type { ProviderOption as HTMLProviderOption, ProviderResult as HTMLProviderResult } from './v1/html-provider-experimental'
-
 export { default as Controller } from './v2/runtime/controller/controller';
 export { default as MPEGTSFeeder } from './v2/runtime/feeder/mpegts-feeder';
 export { default as CanvasMainThreadRenderer } from './v2/runtime/renderer/canvas/canvas-renderer-main';
