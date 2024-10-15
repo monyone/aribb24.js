e1295aeafe943c2fe56fa99dea30d1b9af74d0b2
monyone <monyone.teihen@gmail.com>
2024-10-16 01:11:44 +0900

    Fix: unused code

--- src/v2/tokenizer/b24/jis8/SBTVD/index.ts
+++ src/v2/tokenizer/b24/jis8/SBTVD/index.ts
@@ -11,25 +11,7 @@ const BRAZIL_NORMAL_DICTS = {
 } as const;
 
 // DRCS
-const BRAZIL_DRCS_DICT = {
-  DRCS_0: { type: 'DRCS', code: 0x40, bytes: 2, dict: new Map<number, DRCS>() },
-  DRCS_1: { type: 'DRCS', code: 0x41, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_2: { type: 'DRCS', code: 0x42, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_3: { type: 'DRCS', code: 0x43, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_4: { type: 'DRCS', code: 0x44, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_5: { type: 'DRCS', code: 0x45, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_6: { type: 'DRCS', code: 0x46, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_7: { type: 'DRCS', code: 0x47, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_8: { type: 'DRCS', code: 0x48, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_9: { type: 'DRCS', code: 0x49, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_10: { type: 'DRCS', code: 0x4a, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_11: { type: 'DRCS', code: 0x4b, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_12: { type: 'DRCS', code: 0x4c, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_13: { type: 'DRCS', code: 0x4d, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_14: { type: 'DRCS', code: 0x4e, bytes: 1, dict: new Map<number, DRCS>() },
-  DRCS_15: { type: 'DRCS', code: 0x4f, bytes: 1, dict: new Map<number, DRCS>() },
-  MACRO: { type: 'MACRO', code: 0x70, bytes: 1, dict: new Map<number, ArrayBuffer>([]) },
-} as const;
+const BRAZIL_DRCS_DICT = {} as const;
 
 export default class ARIBBrazilianJIS8Tokenizer extends ARIBB24JIS8Tokenizer {
   public constructor() {
