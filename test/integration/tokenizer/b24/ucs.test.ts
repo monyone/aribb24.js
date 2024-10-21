8b3a499eed53c872fe1287f045f167ada0d8579b
monyone <monyone.teihen@gmail.com>
2024-10-22 04:14:24 +0900

    Fix: More Add Test

--- test/integration/tokenizer/b24/ucs.test.ts
+++ test/integration/tokenizer/b24/ucs.test.ts
@@ -1,15 +1,32 @@
 import { describe, test, expect } from 'vitest';
 import ARIBB24UTF8Tokenizer from '@/v2/tokenizer/b24/ucs/tokenizer';
 import { Character, Space } from '@/v2/tokenizer/token';
+import { CONTROL_CODES } from '@/v2/tokenizer/b24/tokenizer';
+import { UnreachableError } from '@/index';
+
+const generateBinary = (... operation: (number | string)[]): ArrayBuffer => {
+  const encoder = new TextEncoder();
+  const inject = operation.map((elem) => {
+    if (typeof(elem) === 'number') {
+      return Uint8Array.from([elem]);
+    } else{
+      return new Uint8Array(encoder.encode(elem));
+    }
+  });
+
+  const length = inject.reduce((sum, array) => sum + array.byteLength, 0);
+  const uint8 = new Uint8Array(length);
+  for (let i = 0, offset = 0; i < inject.length; offset += inject[i].byteLength, i++) {
+    uint8.set(inject[i], offset);
+  }
+  return uint8.buffer;
+}
 
 describe("ARIB STD-B24 UCS", () => {
-  test('Decode UTF-8 ASCII', () => {
+  test('Decode UTF-8 ASCII as is', () => {
     const tokenizer = new ARIBB24UTF8Tokenizer();
-    const encoder = new TextEncoder();
 
-    const tokens = tokenizer.tokenizeStatement(encoder.encode("This is Test").buffer);
-
-    expect(tokens).toStrictEqual([
+    expect(tokenizer.tokenizeStatement(generateBinary('This is Test'))).toStrictEqual([
       Character.from('T', false),
       Character.from('h', false),
       Character.from('i', false),
@@ -25,13 +42,10 @@ describe("ARIB STD-B24 UCS", () => {
     ]);
   });
 
-  test('Decode UTF-8 2byte String', () => {
+  test('Decode UTF-8 2byte string as is', () => {
     const tokenizer = new ARIBB24UTF8Tokenizer();
-    const encoder = new TextEncoder();
 
-    const tokens = tokenizer.tokenizeStatement(encoder.encode("こんにちは 世界").buffer);
-
-    expect(tokens).toStrictEqual([
+    expect(tokenizer.tokenizeStatement(generateBinary('こんにちは 世界'))).toStrictEqual([
       Character.from('こ', false),
       Character.from('ん', false),
       Character.from('に', false),
@@ -43,14 +57,41 @@ describe("ARIB STD-B24 UCS", () => {
     ]);
   });
 
-  test('Decode UTF-8 Combining Character', () => {
+  test('Decode UTF-8 combining character as is', () => {
     const tokenizer = new ARIBB24UTF8Tokenizer();
-    const encoder = new TextEncoder();
 
-    const tokens = tokenizer.tokenizeStatement(encoder.encode("👨‍👩").buffer);
-
-    expect(tokens).toStrictEqual([
+    expect(tokenizer.tokenizeStatement(generateBinary('👨‍👩'))).toStrictEqual([
       Character.from('👨‍👩', false),
     ]);
-  })
+  });
+
+  test('SS2 throw UnreachableError', () => {
+    const tokenizer = new ARIBB24UTF8Tokenizer();
+
+    expect(() => tokenizer.tokenizeStatement(generateBinary(CONTROL_CODES.SS2))).toThrowError(UnreachableError);
+  });
+
+  test('SS3 throw UnreachableError', () => {
+    const tokenizer = new ARIBB24UTF8Tokenizer();
+
+    expect(() => tokenizer.tokenizeStatement(generateBinary(CONTROL_CODES.SS3))).toThrowError(UnreachableError);
+  });
+
+  test('LS0 throw UnreachableError', () => {
+    const tokenizer = new ARIBB24UTF8Tokenizer();
+
+    expect(() => tokenizer.tokenizeStatement(generateBinary(CONTROL_CODES.LS0))).toThrowError(UnreachableError);
+  });
+
+  test('LS1 throw UnreachableError', () => {
+    const tokenizer = new ARIBB24UTF8Tokenizer();
+
+    expect(() => tokenizer.tokenizeStatement(generateBinary(CONTROL_CODES.LS1))).toThrowError(UnreachableError);
+  });
+
+  test('ESC throw UnreachableError', () => {
+    const tokenizer = new ARIBB24UTF8Tokenizer();
+
+    expect(() => tokenizer.tokenizeStatement(generateBinary(CONTROL_CODES.ESC))).toThrowError(UnreachableError);
+  });
 });
