97832c3a1e51d696f638cc2f544ba54fd2d3de77
monyone <monyone.teihen@gmail.com>
2024-11-07 22:29:13 +0900

    Fix: Format Issue

--- src/lib/encoder/b24/jis8/ARIB/hiragana.ts
+++ src/lib/encoder/b24/jis8/ARIB/hiragana.ts
@@ -4,4 +4,4 @@ import halfwidth from "../halfwidth";
 export default new Map([
   ... Array.from(hiragana.entries()).map(([k, v]) => [v, [k]]) as [string, [number]][],
   ... Array.from(hiragana.entries()).filter(([k, v]) => halfwidth.has(v)).map(([k, v]) => [halfwidth.get(v)!, [k]]) as [string, [number]][],
-])
+]);
--- src/lib/encoder/b24/jis8/ARIB/symbol.ts
+++ src/lib/encoder/b24/jis8/ARIB/symbol.ts
@@ -4,4 +4,4 @@ import unicode from "../../../../tokenizer/b24/jis8/ARIB/symbol-unicode";
 export default new Map([
   ... Array.from(pua.entries()).map(([k, v]) => [v, [(k & 0xFF00) >> 8, (k & 0x00FF) >> 0]]) as [string, [number, number]][],
   ... Array.from(unicode.entries()).map(([k, v]) => [v, [(k & 0xFF00) >> 8, (k & 0x00FF) >> 0]]) as [string, [number, number]][],
-])
+]);
